819d6df5733a9afece5e688166cf4397a8087f2c
Anwesh43 <anweshthecool0@gmail.com>
2019-02-16 00:29:53 +0530

    adding delay as constant

--- tridentrotstepview/src/main/java/com/anwesh/uiprojects/tridentrotstepview/TridentRotStepView.kt
+++ tridentrotstepview/src/main/java/com/anwesh/uiprojects/tridentrotstepview/TridentRotStepView.kt
@@ -21,6 +21,7 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#212121")
 val tridentSizeFactor : Float = 3f
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -102,7 +103,7 @@ class TridentRotStepView(ctx : Context) : View(ctx) {
             if (animated) {
                 cb()
                 try {
-                    Thread.sleep(50)
+                    Thread.sleep(delay)
                     view.invalidate()
                 } catch(ex : Exception) {
 
