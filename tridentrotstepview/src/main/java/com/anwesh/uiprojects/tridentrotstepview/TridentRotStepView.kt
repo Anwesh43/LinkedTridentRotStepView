0c5006f37dd10764792d48f32ba63692c083d780
Anwesh43 <anweshthecool0@gmail.com>
2019-02-15 11:32:46 +0530

    adding TridentRotStepView class

--- tridentrotstepview/src/main/java/com/anwesh/uiprojects/tridentrotstepview/TridentRotStepView.kt
+++ tridentrotstepview/src/main/java/com/anwesh/uiprojects/tridentrotstepview/TridentRotStepView.kt
@@ -35,7 +35,7 @@ fun Canvas.drawTRSNode(i : Int, scale : Float, paint : Paint) {
     val size : Float = gap / sizeFactor
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
-    paint.color = foreColor 
+    paint.color = foreColor
     save()
     translate(w / 2, gap * (i + 1))
     rotate(90f * (1 - 2 * (i % 2)) * scale.divideScale(1, 2))
@@ -49,6 +49,24 @@ fun Canvas.drawTRSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
     restore()
+}
+
+class TridentRotStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTocuhEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
 }
 
 
