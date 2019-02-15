1ef7e780bdebdabb1a2832c634e105deb7179a4a
Anwesh43 <anweshthecool0@gmail.com>
2019-02-15 11:35:43 +0530

    adding State class

--- tridentrotstepview/src/main/java/com/anwesh/uiprojects/tridentrotstepview/TridentRotStepView.kt
+++ tridentrotstepview/src/main/java/com/anwesh/uiprojects/tridentrotstepview/TridentRotStepView.kt
@@ -59,13 +59,33 @@ class TridentRotStepView(ctx : Context) : View(ctx) {
 
     }
 
-    override fun onTocuhEvent(event : MotionEvent) : Boolean {
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
 
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scale.updateValue(dir, trids, 1)
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
 
