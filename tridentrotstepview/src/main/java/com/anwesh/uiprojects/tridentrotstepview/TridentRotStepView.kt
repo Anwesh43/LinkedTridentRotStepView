f53537001ff6505024480a502c1463aeaa8f5a12
Anwesh43 <anweshthecool0@gmail.com>
2019-02-15 23:22:02 +0530

    adding TridentRotStep class

--- tridentrotstepview/src/main/java/com/anwesh/uiprojects/tridentrotstepview/TridentRotStepView.kt
+++ tridentrotstepview/src/main/java/com/anwesh/uiprojects/tridentrotstepview/TridentRotStepView.kt
@@ -159,6 +159,30 @@ class TridentRotStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TridentRotStep(var i : Int) {
+
+        private val root : TRSNode = TRSNode(0)
+        private var curr : TRSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
 
 
