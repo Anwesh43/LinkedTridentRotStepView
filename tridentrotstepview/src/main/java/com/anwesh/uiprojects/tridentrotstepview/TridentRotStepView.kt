c8711be90867495f7cc1b701ac7bfa08dc84179b
Anwesh43 <anweshthecool0@gmail.com>
2019-02-15 10:13:40 +0530

    adding constants

--- tridentrotstepview/src/main/java/com/anwesh/uiprojects/tridentrotstepview/TridentRotStepView.kt
+++ tridentrotstepview/src/main/java/com/anwesh/uiprojects/tridentrotstepview/TridentRotStepView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.tridentrotstepview
 /**
  * Created by anweshmishra on 15/02/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val trids : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#212121")
+
